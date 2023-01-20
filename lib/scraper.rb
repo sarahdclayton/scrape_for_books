19d51329bb34835b417124bc0cf16edc2a893f25
sdclayton <96846005+sdclayton@users.noreply.github.com>
2023-01-19 20:22:17 -0600

    add scraper class

---
+++ lib/scraper.rb
@@ -0,0 +1,27 @@
+# exit to leave pry
+require 'pry'
+# use to parse uri file into ruby hashes
+# Nokogiri::HTML("open-uri link from terminal...uri file")
+require 'nokogiri'
+# turns into a file instance....URI.open("url link")
+require 'open-uri'
+# require_relative 'book.rb'
+
+class Scraper
+    SCRAPE_URL = "http://books.toscrape.com/index.html"
+
+    def self.scrape_book_title 
+        doc = Nokogiri::HTML(URI.open("http://books.toscrape.com/index.html"))
+        side_category_element = doc.css(".side_categories .nav-list")
+        # array of category elements
+        category_elements = side_category_element.css("ul li a")
+        
+        # iterate through category_elements 
+        category_elements.each_with_index do |category_element, i|
+            puts "#{i + 1} #{ category_element.text}"
+        end 
+    end
+end
+
+
+Scraper.scrape_book_title 
